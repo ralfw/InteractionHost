d1c84646b5b08e414f592fef5a6a28e24732ef9b
Ralf Westphal <info@ralfw.de>
2016-01-06 13:43:54 +0100

    self hosting a dummy module

---
+++ source/prototype/ihost/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Nancy;
+
+namespace ihost
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			using (var host = new Nancy.Hosting.Self.NancyHost(new Uri("http://localhost:8080")))
+			{
+				host.Start();
+
+				Console.WriteLine ("Press ENTER to stop...");
+				Console.ReadLine();
+			}
+		}
+	}
+
+
+	public class NancyPortal : NancyModule {
+		public NancyPortal() {
+			Get ["/"] = _ => {
+				Console.WriteLine("called at /");
+				return "Hello: " + DateTime.Now.ToString ();
+			};
+		}
+	}
+}
