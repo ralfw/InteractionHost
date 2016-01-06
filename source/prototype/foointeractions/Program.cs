bb4f7bd3aad4a6d88624a1d46096fc0a42091d0e
Ralf Westphal <info@ralfw.de>
2016-01-06 21:46:33 +0100

    can handle views

---
+++ source/prototype/foointeractions/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace foointeractions
+{
+	class MainClass
+	{
+		public static int Main (string[] args)
+		{
+			using (var p = new ConsoleServicePortal ()) {
+				var input = p.Receive ();
+
+				if (input.IndexOf ("9") >= 0) {
+					p.Send ("errormsg=Bummer!");
+					return 1;
+				} else {
+					p.Send ("time=" + DateTime.Now.ToString ());
+					return 0;
+				}
+			}
+		}
+	}
+
+
+	public class ConsoleServicePortal : IDisposable {
+		public string Receive() {
+			var message = "";
+			var l = "";
+			while ((l = Console.ReadLine()) != null) {
+				if (message != "") message += "\n";
+				message += l;
+			}
+			return message;
+		}
+
+		public void Send(string message) {
+			Console.Write(message);
+		}
+
+		#region IDisposable implementation
+		public void Dispose () {}
+		#endregion
+	}
+}
---
+++ source/prototype/ihost/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using Nancy;
+
+namespace ihost
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var homeUri = new Uri ("http://localhost:8080");
+
+			using (var host = new Nancy.Hosting.Self.NancyHost(homeUri))
+			{
+				host.Start();
+
+				Console.WriteLine ($"Interaction Host listening on {homeUri}");
+				Console.WriteLine ("Press ENTER to stop...");
+				Console.ReadLine();
+			}
+		}
+	}
+
+
+	public class NancyPortal : NancyModule {
+		public NancyPortal() {
+			var interactionLines = System.IO.File.ReadLines ("interactions.txt");
+			var interactions = interactionLines.Select (l => {
+				var lcols = l.Split(';');
+				var responses = lcols.Skip(2).Select(r => {
+					var rcols = r.Split(':');
+					return new {exitcode = int.Parse(rcols[0]), filepath = rcols[1]};
+				});
+				return new {route = lcols[0], cmd = lcols[1], responses = responses};
+			});
+
+
+			foreach (var i in interactions) {
+				Get [i.route] = routeparams => {
+					Console.WriteLine ("route: {0}, cmd: {1}", i.route, i.cmd);
+
+					var input = "";
+					var d = (DynamicDictionary)routeparams;
+					foreach (var k in d.Keys) {
+						if (input != "")
+							input += "\n";
+						input += "route." + k + ":" + d [k];
+					};
+					d = (DynamicDictionary)Request.Query;
+					foreach (var k in d.Keys) {
+						if (input != "")
+							input += "\n";
+						input += "query." + k + ":" + d [k];
+					};
+					d = (DynamicDictionary)Request.Form;
+					foreach (var k in d.Keys) {
+						if (input != "")
+							input += "\n";
+						input += "form." + k + ":" + d [k];
+					};
+						
+					Console.WriteLine(input);
+
+					var cmdParts = i.cmd.Split(new[]{' '}, 2);
+					Console.WriteLine("run {0} with: {1}", cmdParts[0], cmdParts[1]);
+
+					int exitCode = 0;
+					var output = "";
+					using(var cli = new ConsoleServiceProvider(cmdParts[0], cmdParts[1])) {
+						cli.Process(input, out exitCode, out output);
+						Console.WriteLine("exited with: {0}", exitCode);
+						Console.WriteLine(output);
+					}
+
+					var response = i.responses.FirstOrDefault(r => r.exitcode == exitCode);
+					if (response != null) {
+						if (response.filepath.StartsWith("Content/", StringComparison.InvariantCultureIgnoreCase))
+							return Response.AsRedirect(response.filepath);
+						else {
+							// convert output into view model
+							return View[response.filepath];
+						}
+					}
+					else
+						return output;
+				};
+			}
+		}
+	}
+
+}
