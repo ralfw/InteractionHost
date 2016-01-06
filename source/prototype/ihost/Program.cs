ed1938265ed3593f228a9f1d77f51b97aa882bd2
Ralf Westphal <info@ralfw.de>
2016-01-06 17:36:39 +0100

    reading routes from file, redirecting to static content

--- source/prototype/ihost/Program.cs
+++ source/prototype/ihost/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Nancy;
 
 namespace ihost
@@ -23,6 +25,48 @@ namespace ihost
 
 	public class NancyPortal : NancyModule {
 		public NancyPortal() {
+			var interactionLines = System.IO.File.ReadLines ("interactions.txt");
+			var interactions = interactionLines.Select (l => {
+				var lcols = l.Split(';');
+				var responses = lcols.Skip(2).Select(r => {
+					var rcols = r.Split(':');
+					return new {exitcode = rcols[0], filepath = rcols[1]};
+				});
+				return new {route = lcols[0], cmd = lcols[1], responses = responses};
+			});
+
+
+			foreach (var i in interactions) {
+				Get [i.route] = routeparams => {
+					Console.WriteLine ("route: {0}, cmd: {1}", i.route, i.cmd);
+
+					var input = "";
+					var d = (DynamicDictionary)routeparams;
+					foreach (var k in d.Keys) {
+						if (input != "")
+							input += "\n";
+						input += "route." + k + ":" + d [k];
+					};
+					d = (DynamicDictionary)Request.Query;
+					foreach (var k in d.Keys) {
+						if (input != "")
+							input += "\n";
+						input += "query." + k + ":" + d [k];
+					};
+					d = (DynamicDictionary)Request.Form;
+					foreach (var k in d.Keys) {
+						if (input != "")
+							input += "\n";
+						input += "form." + k + ":" + d [k];
+					};
+
+					Console.WriteLine(input);
+
+					return Response.AsRedirect(i.responses.First().filepath);
+				};
+			}
+
+
 			Get ["/{a}/{b}"] = p => {
 				var d = (DynamicDictionary)p;
 				Console.WriteLine("{0}", string.Join(",", d.Keys));
