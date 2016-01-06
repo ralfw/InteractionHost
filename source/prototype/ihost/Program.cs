593604ab7c1aa4449258f99f3f8d7300b984e8cd
Ralf Westphal <info@ralfw.de>
2016-01-06 16:57:42 +0100

    access route and query params thru dynamic dictionaries

--- source/prototype/ihost/Program.cs
+++ source/prototype/ihost/Program.cs
@@ -7,10 +7,13 @@ namespace ihost
 	{
 		public static void Main (string[] args)
 		{
-			using (var host = new Nancy.Hosting.Self.NancyHost(new Uri("http://localhost:8080")))
+			var homeUri = new Uri ("http://localhost:8080");
+
+			using (var host = new Nancy.Hosting.Self.NancyHost(homeUri))
 			{
 				host.Start();
 
+				Console.WriteLine ($"Interaction Host listening on {homeUri}");
 				Console.WriteLine ("Press ENTER to stop...");
 				Console.ReadLine();
 			}
@@ -20,8 +23,14 @@ namespace ihost
 
 	public class NancyPortal : NancyModule {
 		public NancyPortal() {
-			Get ["/"] = _ => {
-				Console.WriteLine("called at /");
+			Get ["/{a}/{b}"] = p => {
+				var d = (DynamicDictionary)p;
+				Console.WriteLine("{0}", string.Join(",", d.Keys));
+
+				d = (DynamicDictionary)Request.Query;
+				Console.WriteLine("{0}", string.Join(",", d.Keys));
+
+				Console.WriteLine("called at /{0}/{1}?id={2}", p.a, p.b, Request.Query["id"]);
 				return "Hello: " + DateTime.Now.ToString ();
 			};
 		}
